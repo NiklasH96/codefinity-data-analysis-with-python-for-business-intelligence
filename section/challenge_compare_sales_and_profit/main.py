c4ba8546138e8b05ce7e75619408128ed6fdf033
NiklasH96 <>
2026-07-01 13:48:07 +0000

    Challenge: Compare Sales and Profit

---
+++ section/challenge_compare_sales_and_profit/main.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+def compare_sales_profit(sales_list, profit_list, product_names):
+    fig, axes = plt.subplots(1, 2)
+
+    axes[0].bar(product_names, sales_list)
+    axes[0].set_title('Sales by Product')
+    axes[0].set_xlabel('Product')
+    axes[0].set_ylabel('Sales')
+
+    axes[1].bar(product_names, profit_list)
+    axes[1].set_title('Profit by Product')
+    axes[1].set_xlabel('Product')
+    axes[1].set_ylabel('Profit')
+
+# Sample calls
+sales = [1000, 1500, 800, 2000]
+profits = [200, 300, 100, 400]
+products = ['A', 'B', 'C', 'D']
+compare_sales_profit(sales, profits, products)
